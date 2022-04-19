f594e2834b5dce822e481ba3e5a9f9e2ad377cb7
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-19 16:31:38 -0700

    fix index routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
-const recipeRoutes = require("./recipeRoutes");
+const postRoutes = require("./post-routes");
 
 router.use("/users", userRoutes);
-router.use("/recipes", recipeRoutes);
+router.use("/post", postRoutes);
 
 module.exports = router;
