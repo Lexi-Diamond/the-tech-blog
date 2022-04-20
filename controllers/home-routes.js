25d793b4e772b2788ca0e18336cca5d739a61987
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-20 09:42:57 -0700

    add login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,6 +42,16 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get("/login", (req, res) => {
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
 
 
 
