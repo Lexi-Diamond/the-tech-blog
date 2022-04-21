d0862a64314af8ecbbafaf798066ed23cbc94e69
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-21 15:58:14 -0700

    added to commentjs and edit-postjs

---
+++ public/js/edit-post.js
@@ -0,0 +1,28 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="title"]').value;
+    const post_content = document.querySelector('input[name="post_body"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            post_body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
